120a5c65a5c16ff6156b4672c756326d531d5ba8
phillcent <45963417+phillcent@users.noreply.github.com>
2024-10-22 18:23:21 -0400

    fourt commit

--- base_by_phill/main.tf
+++ base_by_phill/main.tf
@@ -3,9 +3,9 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = "us-east-1"
+  
+  region     = var.aws_region
+  profile = "my-profile"
 }
 
 ##################################################################################
@@ -13,7 +13,7 @@ provider "aws" {
 ##################################################################################
 
 data "aws_ssm_parameter" "amzn2_linux" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  name = "/myapp/latest/amazon-linux2-ami"
 }
 
 ##################################################################################
@@ -22,20 +22,23 @@ data "aws_ssm_parameter" "amzn2_linux" {
 
 # NETWORKING #
 resource "aws_vpc" "app" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
 
+  tags = local.common_tags
 }
 
 resource "aws_internet_gateway" "app" {
   vpc_id = aws_vpc.app.id
-
+  tags   = local.common_tags
 }
 
 resource "aws_subnet" "public_subnet1" {
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = var.vpc_public_subnet1_cidr_block
   vpc_id                  = aws_vpc.app.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = local.common_tags
 }
 
 # ROUTING #
@@ -46,6 +49,8 @@ resource "aws_route_table" "app" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.app.id
   }
+
+  tags = local.common_tags
 }
 
 resource "aws_route_table_association" "app_subnet1" {
@@ -74,14 +79,17 @@ resource "aws_security_group" "nginx_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = local.common_tags
 }
 
 # INSTANCES #
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type          = "t3.micro"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
 
   user_data = <<EOF
 #! /bin/bash
@@ -90,6 +98,8 @@ sudo service nginx start
 sudo rm /usr/share/nginx/html/index.html
 echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
 EOF
+
+  tags = local.common_tags
 
 }
 
--- base_by_phill/variables.tf
+++ base_by_phill/variables.tf
@@ -1,13 +1,45 @@
-variable "access_key" {
-    type = string
-    description = "value"
-    sensitive = true
-  
-}
 
-variable "secret_key" {
-    type = string
-    description = "value"
-    sensitive = true
-  
+
+variable "aws_region" {
+  type        = string
+  description = "value"
+  default     = "ca-central-1"
+}
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "value"
+  default     = true
+}
+variable "vpc_cidr_block" {
+  type        = string
+  description = "value"
+  default     = "10.0.0.0/16"
+}
+variable "vpc_public_subnet1_cidr_block" {
+  type        = string
+  description = "value"
+  default     = "10.0.0.0/24"
+}
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "value"
+  default     = true
+}
+variable "instance_type" {
+  type        = string
+  description = "value"
+  default     = "t2.micro"
+}
+variable "company" {
+  type    = string
+  default = "Phill_test"
+
+}
+variable "project" {
+  type        = string
+  description = "value"
+}
+variable "billing_code" {
+  type        = string
+  description = "value"
 }
